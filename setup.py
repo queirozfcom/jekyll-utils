d85c043860d1a09aeb2741a4a5fb276d20b9ed44
Felipe Almeida <queirozfcom@gmail.com>
2016-12-24 02:03:30 -0200

    this had stopped working in python 3.5... just converting the name to lowercase made it work again fix #9

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-        name="JekyllUtils",
+        name="jekyllutils",
         version='0.1',
         py_modules=['generators'],
         install_requires=[
